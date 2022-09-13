994b31b7feeee8c35f299d9bcdedfbb81ac49b65
kyeyunedaniel <kyeyunedaniel0@gmail.com>
2022-09-13 12:33:26 +0300

    first

---
+++ even.js
@@ -0,0 +1,15 @@
+function num(){
+    for(var i=0; i<=100; i+=10){
+        if(i%2==0){
+            console.log(i)
+        }
+    }
+
+
+}
+num();
+
+
+//function to print odd numbers
+// Language: javascript
+// Path: odd.js
