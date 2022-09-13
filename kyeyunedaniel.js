c94c60188da3b88c56cbed0935dd6b6c19efb0d0
kyeyunedaniel <kyeyunedaniel0@gmail.com>
2022-09-13 11:16:41 +0300

    Kyeyune Daniel

---
+++ kyeyunedaniel.js
@@ -0,0 +1,69 @@
+let you =" are the reason";
+// This allows you declare a variable that cannot be redeclared and assigns it a value of "are the reason" and the variable is called you
+var we = " the next generation of programmer";
+//this allows you declare a variable that can be redeclared and assigns it a value of "the next generation of programmer" and the variable is called we
+const us = ' we are upcoming software problem solvers';
+//This allows you declare a variable that is a constant and assigns it a value of "we are upcoming software problem solvers" and the variable is called us
+let num1 = 5;
+// This allows you declare a variable that cannot be redeclared and assigns it a value of 5 and the variable is called num1
+let num2 = 10;
+// This allows you declare a variable that cannot be redeclared and assigns it a value of 10 and the variable is called num2
+let num3 = num2 += num1;
+// This allows you declare a variable that cannot be redeclared and assigns it a value of num2 += num1 and the variable is called num3.
+//this allows you to add num1 to num2 and assign the value to num3. 
+let ans = num1 % num2;
+// This allows you declare a variable that cannot be redeclared and assigns it a value of num1 % num2 and the variable is called ans.
+// This allows you to divide num1 by num2 and assign the remainder to ans.
+console.log(ans);
+// This allows you to print the value of ans to the console.
+console.log(num3 *= 2);
+// This allows you to multiply num3 by 2 and assign the value to num3.
+//The reult is then printed to the console.
+var num4;
+//this allows you declare a variable that can be redeclared and assigns it a value of undefined and the variable is called num4
+console.log(num4);
+// This allows you to print the value of num4 to the console.
+console.log(2num6);
+// This allows you to print the value of 2num6 to the console.
+// This will result in an error because a variable cannot start with a number.
+// This is a syntax error.  Num6 has also not been declared/ defined.
+var ans = 'this is not right';
+// This allows you declare a variable that can be redeclared and assigns it a value of 'this is not right' and the variable is called ans.
+// This is a syntax error because a variable cannot be declared twice. since ans has already been declared using the const let. 
+var let ='do you know what you are doing?';
+//this allows you declare a variable that can be redeclared and assigns it a value of 'do you know what you are doing?' and the variable is called let.
+// Since let is a keyword/reserve name, we should avoid using reserve words as variable names. This is supposed to result to a syntax error but when i run it, the code was working
+const dad;
+// This allows you declare a variable that is a constant and assigns it a value of undefined and the variable is called dad.
+const myNum = num1>num2? num3:us;
+// This allows you declare a variable that is a constant and assigns it a value of num1>num2? num3:us and the variable is called myNum.
+// This is a ternary operator. It checks if num1 is greater than num2. If it is, it assigns num3 to myNum. If it is not, it assigns us to myNum.
+console.log(--num3);
+// This allows you to decrement num3 by 1 and assign the value to num3.
+console.log(num3);
+// this allows you to print the value of num3 to the console.
+console.log(num3++);
+//this allows you to print the value of num3 to the console and then increment num3 by 1.
+console.log(num2 != num3);
+// this allows you to check if num2 is not equal to num3 and assign the value to num2 != num3.
+// This will result in a boolean value of true printed out in the console.
+console.log(num3 !=num2 && num2==num2);
+//this allows you to check if num3 is not equal to num2 and num2 is equal to num2 and assign the value to num3 !=num2 && num2==num2.
+// This will result in a boolean value of true printed out in the console. 
+// Both conditions must be true for the result to be true.
+
+if(100>=200){
+	const num2 =20;
+	console.log('you are now a junior developer');
+}
+
+
+// This allows you to check if 100 is greater than or equal to 200. 
+// If it is, it assigns 20 to num2 and prints out 'you are now a junior developer' to the console.
+
+
+
+
+
+
+
